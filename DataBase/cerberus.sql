2fbb6424e76ac737ae3321da374a682b812b7655
ebenezerv99 <ebenezerv99@gmail.com>
2019-09-10 13:59:23 +0530

    database edits

--- DataBase/cerberus.sql
+++ DataBase/cerberus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Sep 10, 2019 at 06:49 AM
+-- Generation Time: Sep 10, 2019 at 08:28 AM
 -- Server version: 5.7.26
 -- PHP Version: 7.2.18
 
@@ -30,12 +30,14 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `attendance`;
 CREATE TABLE IF NOT EXISTS `attendance` (
-  `attendanceID` int(6) NOT NULL AUTO_INCREMENT,
+  `attendanceID` int(5) NOT NULL AUTO_INCREMENT,
   `PRN` bigint(16) NOT NULL,
-  `scheduleID` varchar(6) NOT NULL,
-  `timeID` varchar(5) NOT NULL,
+  `scheduleID` int(4) NOT NULL,
+  `dateID` int(3) NOT NULL,
+  `timeID` int(5) NOT NULL,
   PRIMARY KEY (`attendanceID`),
-  KEY `PRN` (`PRN`,`scheduleID`,`timeID`)
+  KEY `PRN` (`PRN`,`scheduleID`,`timeID`),
+  KEY `dateID` (`dateID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -46,7 +48,7 @@ CREATE TABLE IF NOT EXISTS `attendance` (
 
 DROP TABLE IF EXISTS `batch`;
 CREATE TABLE IF NOT EXISTS `batch` (
-  `batchID` varchar(2) NOT NULL,
+  `batchID` tinyint(1) NOT NULL,
   `name` varchar(6) NOT NULL,
   PRIMARY KEY (`batchID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -59,7 +61,7 @@ CREATE TABLE IF NOT EXISTS `batch` (
 
 DROP TABLE IF EXISTS `class`;
 CREATE TABLE IF NOT EXISTS `class` (
-  `classID` int(1) NOT NULL,
+  `classID` tinyint(1) NOT NULL,
   `class` varchar(12) NOT NULL,
   PRIMARY KEY (`classID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -67,12 +69,12 @@ CREATE TABLE IF NOT EXISTS `class` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `datestore`
+-- Table structure for table `datedata`
 --
 
-DROP TABLE IF EXISTS `datestore`;
-CREATE TABLE IF NOT EXISTS `datestore` (
-  `dateID` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `datedata`;
+CREATE TABLE IF NOT EXISTS `datedata` (
+  `dateID` int(3) NOT NULL AUTO_INCREMENT,
   `date` date NOT NULL,
   PRIMARY KEY (`dateID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -80,12 +82,12 @@ CREATE TABLE IF NOT EXISTS `datestore` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `day`
+-- Table structure for table `daydata`
 --
 
-DROP TABLE IF EXISTS `day`;
-CREATE TABLE IF NOT EXISTS `day` (
-  `dayID` int(4) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `daydata`;
+CREATE TABLE IF NOT EXISTS `daydata` (
+  `dayID` varchar(3) NOT NULL,
   `dayOfWeek` varchar(9) NOT NULL,
   PRIMARY KEY (`dayID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -98,33 +100,33 @@ CREATE TABLE IF NOT EXISTS `day` (
 
 DROP TABLE IF EXISTS `faculty`;
 CREATE TABLE IF NOT EXISTS `faculty` (
-  `facultyID` int(6) NOT NULL,
-  `email` varchar(40) NOT NULL,
+  `facultyID` int(3) NOT NULL AUTO_INCREMENT,
   `name` varchar(120) NOT NULL,
+  `email` varchar(50) NOT NULL,
   `password` varchar(256) NOT NULL,
   PRIMARY KEY (`facultyID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=102 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `faculty`
 --
 
-INSERT INTO `faculty` (`facultyID`, `email`, `name`, `password`) VALUES
+INSERT INTO `faculty` (`facultyID`, `name`, `email`, `password`) VALUES
 (101, 'ebenezerv99@gmail.com', 'Vraj Kotwala', 'de786632264b9f3a79a7ec15c2460dd079554f0b0ad377cb8408edeb26947eec');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `fingerprints`
+-- Table structure for table `facultyfingerprint`
 --
 
-DROP TABLE IF EXISTS `fingerprints`;
-CREATE TABLE IF NOT EXISTS `fingerprints` (
-  `PRN` bigint(16) NOT NULL,
+DROP TABLE IF EXISTS `facultyfingerprint`;
+CREATE TABLE IF NOT EXISTS `facultyfingerprint` (
+  `FacultyID` int(3) NOT NULL,
   `templateID` tinyint(1) NOT NULL,
   `template` blob NOT NULL,
-  PRIMARY KEY (`PRN`,`templateID`),
-  KEY `PRN` (`PRN`)
+  PRIMARY KEY (`FacultyID`,`templateID`),
+  KEY `PRN` (`FacultyID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -135,7 +137,7 @@ CREATE TABLE IF NOT EXISTS `fingerprints` (
 
 DROP TABLE IF EXISTS `lab`;
 CREATE TABLE IF NOT EXISTS `lab` (
-  `labID` varchar(2) NOT NULL,
+  `labID` tinyint(1) NOT NULL,
   `name` varchar(4) NOT NULL,
   PRIMARY KEY (`labID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -149,19 +151,27 @@ CREATE TABLE IF NOT EXISTS `lab` (
 DROP TABLE IF EXISTS `log`;
 CREATE TABLE IF NOT EXISTS `log` (
   `logID` int(4) NOT NULL AUTO_INCREMENT,
-  `logType` varchar(15) NOT NULL,
-  `dateID` int(11) NOT NULL,
+  `logTypeID` tinyint(1) NOT NULL,
+  `dateID` int(3) NOT NULL,
   `timeID` int(5) NOT NULL,
-  `comments` varchar(256) NOT NULL,
-  PRIMARY KEY (`logID`)
+  `comments` varchar(200) NOT NULL,
+  PRIMARY KEY (`logID`),
+  KEY `dateID` (`dateID`,`timeID`),
+  KEY `logTypeID` (`logTypeID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `log`
+-- Table structure for table `logtypeid`
 --
 
-INSERT INTO `log` (`logID`, `logType`, `dateID`, `timeID`, `comments`) VALUES
-(1, 'fingerprints', 0, 0, 'enrolled finger 1 for 2017033800104472');
+DROP TABLE IF EXISTS `logtypeid`;
+CREATE TABLE IF NOT EXISTS `logtypeid` (
+  `logTypeID` tinyint(1) NOT NULL,
+  `logType` varchar(15) NOT NULL,
+  PRIMARY KEY (`logTypeID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -171,16 +181,19 @@ INSERT INTO `log` (`logID`, `logType`, `dateID`, `timeID`, `comments`) VALUES
 
 DROP TABLE IF EXISTS `otp`;
 CREATE TABLE IF NOT EXISTS `otp` (
+  `OTPID` int(4) NOT NULL AUTO_INCREMENT,
   `PRN` bigint(16) NOT NULL,
-  `OTP` bigint(16) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `OTP` varchar(256) NOT NULL,
+  PRIMARY KEY (`OTPID`),
+  KEY `PRN` (`PRN`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `otp`
 --
 
-INSERT INTO `otp` (`PRN`, `OTP`) VALUES
-(2017033800107501, 223465);
+INSERT INTO `otp` (`OTPID`, `PRN`, `OTP`) VALUES
+(1, 2017033800107501, '223465');
 
 -- --------------------------------------------------------
 
@@ -190,11 +203,12 @@ INSERT INTO `otp` (`PRN`, `OTP`) VALUES
 
 DROP TABLE IF EXISTS `rollcall`;
 CREATE TABLE IF NOT EXISTS `rollcall` (
-  `classID` int(1) NOT NULL,
+  `classID` tinyint(1) NOT NULL,
   `rollNo` varchar(3) NOT NULL,
   `PRN` bigint(16) NOT NULL,
   PRIMARY KEY (`classID`,`rollNo`),
-  KEY `PRN` (`PRN`)
+  KEY `PRN` (`PRN`),
+  KEY `classID` (`classID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -205,10 +219,10 @@ CREATE TABLE IF NOT EXISTS `rollcall` (
 
 DROP TABLE IF EXISTS `slot`;
 CREATE TABLE IF NOT EXISTS `slot` (
-  `sessionID` varchar(5) NOT NULL,
+  `slotID` tinyint(1) NOT NULL,
   `startTime` time NOT NULL,
   `endTime` time NOT NULL,
-  PRIMARY KEY (`sessionID`)
+  PRIMARY KEY (`slotID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -221,7 +235,7 @@ DROP TABLE IF EXISTS `student`;
 CREATE TABLE IF NOT EXISTS `student` (
   `PRN` bigint(16) NOT NULL,
   `name` varchar(120) NOT NULL,
-  `email` varchar(40) NOT NULL,
+  `email` varchar(50) NOT NULL,
   `password` varchar(256) NOT NULL,
   PRIMARY KEY (`PRN`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -236,16 +250,32 @@ INSERT INTO `student` (`PRN`, `name`, `email`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `studentfingerprint`
+--
+
+DROP TABLE IF EXISTS `studentfingerprint`;
+CREATE TABLE IF NOT EXISTS `studentfingerprint` (
+  `PRN` bigint(16) NOT NULL,
+  `templateID` tinyint(1) NOT NULL,
+  `template` blob NOT NULL,
+  PRIMARY KEY (`PRN`,`templateID`),
+  KEY `PRN` (`PRN`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `studentsubject`
 --
 
 DROP TABLE IF EXISTS `studentsubject`;
 CREATE TABLE IF NOT EXISTS `studentsubject` (
   `PRN` bigint(16) NOT NULL,
-  `subjectID` int(8) NOT NULL,
-  `batchID` int(2) NOT NULL,
+  `subjectID` int(7) NOT NULL,
+  `batchID` tinyint(1) NOT NULL,
   PRIMARY KEY (`PRN`,`subjectID`),
-  KEY `batchID` (`batchID`)
+  KEY `batchID` (`batchID`),
+  KEY `PRN` (`PRN`,`subjectID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -256,7 +286,7 @@ CREATE TABLE IF NOT EXISTS `studentsubject` (
 
 DROP TABLE IF EXISTS `subject`;
 CREATE TABLE IF NOT EXISTS `subject` (
-  `subjectID` varchar(8) NOT NULL,
+  `subjectID` varchar(7) NOT NULL,
   `sem` tinyint(1) NOT NULL,
   `subject` varchar(40) NOT NULL,
   `classID` int(1) NOT NULL,
@@ -285,45 +315,17 @@ CREATE TABLE IF NOT EXISTS `timedata` (
 
 DROP TABLE IF EXISTS `timetable`;
 CREATE TABLE IF NOT EXISTS `timetable` (
-  `scheduleID` varchar(4) NOT NULL,
-  `sessionID` varchar(5) NOT NULL,
-  `labID` varchar(2) NOT NULL,
-  `subjectID` varchar(8) NOT NULL,
-  `batchID` varchar(2) NOT NULL,
-  `facultyID` varchar(6) NOT NULL,
-  `weekID` int(3) NOT NULL,
-  `dayID` varchar(4) NOT NULL,
+  `scheduleID` int(4) NOT NULL AUTO_INCREMENT,
+  `slotID` int(1) NOT NULL,
+  `labID` int(1) NOT NULL,
+  `subjectID` varchar(7) NOT NULL,
+  `batchID` int(1) NOT NULL,
+  `facultyID` int(3) NOT NULL,
+  `weekID` int(2) NOT NULL,
+  `dayID` varchar(3) NOT NULL,
   PRIMARY KEY (`scheduleID`),
-  KEY `sessionID` (`sessionID`,`labID`,`subjectID`,`batchID`,`facultyID`,`dayID`)
+  KEY `sessionID` (`slotID`,`labID`,`subjectID`,`batchID`,`facultyID`,`dayID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `timetable`
---
-
-INSERT INTO `timetable` (`scheduleID`, `sessionID`, `labID`, `subjectID`, `batchID`, `facultyID`, `weekID`, `dayID`) VALUES
-('s1', 'sess1', 'L1', 'BCA1538', 'b1', '116', 0, '1'),
-('s10', 'sess5', 'L2', 'BCA1001', 'b2', '109', 0, '1'),
-('s11', 'sess5', 'L3', 'BCA1001', 'b3', '110', 0, '1'),
-('s12', 'sess1', 'L1', 'BCA1538', 'b2', '116', 0, '2'),
-('s13', 'sess2', 'L1', 'BCA1539', 'b2', '116', 0, '2'),
-('s14', 'sess2', 'L2', 'BCA1530', 'b3', '102', 0, '2'),
-('s15', 'sess2', 'L3', 'BCA1501', 'b2', '103', 0, '2'),
-('s16', 'sess3', 'L1', 'BCA1403', 'b3', '104', 0, '2'),
-('s17', 'sess4', 'L1', 'BCA1010', 'b2', '105', 0, '2'),
-('s18', 'sess4', 'L2', 'BCA1307', 'b3', '106', 0, '2'),
-('s19', 'sess4', 'L3', 'BCA1208', 'b2', '107', 0, '2'),
-('s2', 'sess2', 'L1', 'BCA1301', 'b2', '116', 0, '1'),
-('s20', 'sess5', 'L1', 'BCA1105', 'b3', '108', 0, '2'),
-('s21', 'sess5', 'L2', 'BCA1106', 'b2', '109', 0, '2'),
-('s22', 'sess5', 'L3', 'BCA1001', 'b3', '110', 0, '2'),
-('s3', 'sess2', 'L2', 'BCA1308', 'b3', '102', 0, '1'),
-('s4', 'sess2', 'L3', 'BCA1303', 'b2', '103', 0, '1'),
-('s5', 'sess3', 'L1', 'BCA1539', 'b3', '104', 0, '1'),
-('s6', 'sess4', 'L1', 'BCA1105', 'b2', '105', 0, '1'),
-('s7', 'sess4', 'L2', 'BCA1106', 'b3', '106', 0, '1'),
-('s8', 'sess4', 'L3', 'BCA1208', 'b2', '107', 0, '1'),
-('s9', 'sess5', 'L1', 'BCA1001', 'b3', '108', 0, '1');
 
 -- --------------------------------------------------------
 
@@ -333,8 +335,8 @@ INSERT INTO `timetable` (`scheduleID`, `sessionID`, `labID`, `subjectID`, `batch
 
 DROP TABLE IF EXISTS `week`;
 CREATE TABLE IF NOT EXISTS `week` (
-  `weekID` int(3) NOT NULL AUTO_INCREMENT,
-  `week` varchar(7) NOT NULL,
+  `weekID` int(2) NOT NULL AUTO_INCREMENT,
+  `week` int(2) NOT NULL,
   PRIMARY KEY (`weekID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 COMMIT;
--- WebApp/src/java/homepage.java
+++ WebApp/src/java/homepage.java
@@ -1,8 +1,15 @@
 
+import com.mysql.cj.jdbc.result.ResultSetMetaData;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -37,10 +44,55 @@ public class homepage extends HttpServlet {
                         + "<div class=\"col-md-12\">\n"
                         + "<div class=\"container my-5\" style=\"padding: 0 70px;\">\n"
                         + "");*/
+                String[][] slots = null;
+                int no_of_slots = 0;
+                try {
+                    Class.forName("com.mysql.cj.jdbc.Driver");
+                    Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cerberus?zeroDateTimeBehavior=convertToNull", "root", "");
+                    PreparedStatement ps = con.prepareStatement("select * from slot order by startTime");
+                    ResultSet rs = ps.executeQuery();
+                    while(rs.next())
+                    {
+                        slots[no_of_slots][0]=rs.getString(1);
+                        slots[no_of_slots][1]=rs.getString(2);
+                        slots[no_of_slots][2]=rs.getString(3);
+                        no_of_slots++;
+                    }
+                    con.close();
+                } catch (ClassNotFoundException | SQLException e) {
+                    RequestDispatcher rd = request.getRequestDispatcher("message.jsp");
+                    request.setAttribute("redirect", "true");
+                    request.setAttribute("head", "Database Error");
+                    request.setAttribute("body", e.getMessage());
+                    request.setAttribute("url", "index.html");
+                    request.setAttribute("sec", "2");
+                    rd.forward(request, response);
+                }
                 Date date = new Date();
                 SimpleDateFormat ft = new SimpleDateFormat("w");
-                System.out.println("Current Week: " + ft.format(date));
-
+                int weekID=Integer.parseInt(ft.format(date));
+                String[][] timetable = new String[8][no_of_slots];
+                try {
+                    Class.forName("com.mysql.cj.jdbc.Driver");
+                    Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cerberus?zeroDateTimeBehavior=convertToNull", "root", "");
+                    PreparedStatement ps = con.prepareStatement("select * from timetable where weekID=?");
+                    ps.setInt(1,weekID);
+                    ResultSet rs = ps.executeQuery();
+                    while(rs.next())
+                    {
+                        no_of_slots++;
+                    }
+                    con.close();
+                } catch (ClassNotFoundException | SQLException e) {
+                    RequestDispatcher rd = request.getRequestDispatcher("message.jsp");
+                    request.setAttribute("redirect", "true");
+                    request.setAttribute("head", "Database Error");
+                    request.setAttribute("body", e.getMessage());
+                    request.setAttribute("url", "index.html");
+                    request.setAttribute("sec", "2");
+                    rd.forward(request, response);
+                }
+                
             } else {
                 out.print("Student Panel </li>"
                         + "<li> <a href=\"#\"> <i class=\"far fa-calendar-alt\"></i> &nbsp; Attendance</a> </li>\n"
