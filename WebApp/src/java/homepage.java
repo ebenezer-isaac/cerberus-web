e38a683d5c0488e7eab61ac06f98f3fed422273f
Ebenezer Isaac <ebenezerv99@gmail.com>
2019-10-04 16:37:21 +0530

    sidebar external file

--- WebApp/src/java/homepage.java
+++ WebApp/src/java/homepage.java
@@ -24,40 +24,20 @@ public class homepage extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             request.getRequestDispatcher("nav.html").include(request, response);
-            HttpSession session = request.getSession(true);
+                        HttpSession session = request.getSession();
             int access = (int) session.getAttribute("access");
             if (access == 1) {
-                out.print("Faculty Panel </li>"
-                        + "<li> <a href=\"/Cerberus/editTimetable\"> <i class=\"far fa-calendar-alt\"></i> &nbsp; Timetable Management </a> </li>\n"
-                        + "<li> <a href=\"/Cerberus/dispSubject\"> <i class=\"fas fa-list\"></i> &nbsp;  Subjects Management</a> </li>\n"
-                        + "<li> <a href=\"#\"> <i class=\"far fa-list-alt\"></i> &nbsp;  Student Management</a> </li>\n"
-                        + "<li> <a href=\"#\"> <i class=\"far fa-folder-open\"></i> &nbsp;  Attendance Management </a> </li>\n"
-                        + "<li> <a href=\"#\"> <i class=\"fas fa-user-cog\"></i> &nbsp; Admin Management </a> </li>\n"
-                        + "<li> <a href=\"#\"> <i class=\"fas fa-upload\"></i> &nbsp; Student Progression </a> </li>\n"
-                        + "<li> <a href=\"#\"> <i class=\"fas fa-braille\"></i> &nbsp; Device OTP </a> </li>\n"
-                        + "<li> <a href=\"#\"> <i class=\"fas fa-braille\"></i> &nbsp; Profile </a> </li>\n"
-                        + "</ul></div>");
-                out.print("<div class=\"page-content-wrapper\">\n"
-                        + "<button class=\"btn btn-link\" id=\"menu-toggle\" style=\"background-color: #0d0d0d;\"> <i class=\"fas fa-align-justify\" style=\"color: white;\"></i> </button>\n"
-                        + "<div class=\"row\">"
-                        + "<div class=\"col-md-12\">\n"
-                        + "<div class=\"container my-5\" style=\"padding: 0 70px;\">\n"
-                        + "");
-
-                out.print("LAB 1");
-                out.println(printTimetable(1));
-                out.print("LAB 2");
-                out.println(printTimetable(2));
-                out.print("LAB 3");
-                out.println(printTimetable(3));
-                out.println("</div></div></div></div></div><script src=\"js/Sidebar-Menu.js\"></script><script src=\"js/main.js\"></script>");
-
+                request.getRequestDispatcher("side-faculty.html").include(request, response);
             } else {
-                out.print("Student Panel </li>"
-                        + "<li> <a href=\"#\"> <i class=\"far fa-calendar-alt\"></i> &nbsp; Attendance</a> </li>\n"
-                        + "<li> <a href=\"#\"> <i class=\"fas fa-list\"></i> &nbsp; Profile </a> </li>\n"
-                        + "</ul></div>");
+                request.getRequestDispatcher("side-student.html").include(request, response);
             }
+            out.print("LAB 1");
+            out.println(printTimetable(1));
+            out.print("LAB 2");
+            out.println(printTimetable(2));
+            out.print("LAB 3");
+            out.println(printTimetable(3));
+            out.println("</div></div></div></div></div><script src=\"js/Sidebar-Menu.js\"></script><script src=\"js/main.js\"></script>");
         }
     }
 
