727a9bb27490179ac43a9f6161e068660a332be7
Ebenezer Isaac <ebenezerv99@gmail.com>
2019-10-22 23:05:14 +0530

    profile pic

--- WebApp/src/java/NewServlet.java
+++ WebApp/src/java/NewServlet.java
@@ -1,36 +1,31 @@
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
 import javax.xml.bind.DatatypeConverter;
 
-@MultipartConfig(maxFileSize = 16177215)
-public class editProfile extends HttpServlet {
+public class NewServlet extends HttpServlet {
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            String name = request.getParameter("firstname");
-            Part filePart = request.getPart("avatar-file");
-            InputStream inputStream = null;
-            if (filePart != null) {
-                inputStream = filePart.getInputStream();
-            }
+            URL url = new URL("http://msubcdndwn.digitaluniversity.ac/resources/public/msub/Photosign/Photo/2017/D17CJ0002566_P.JPG");
+            InputStream inputStream = url.openStream();
             try {
                 Class.forName("com.mysql.cj.jdbc.Driver");
                 Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb?zeroDateTimeBehavior=convertToNull", "root", "");
                 PreparedStatement ps1 = con.prepareStatement("INSERT INTO profile_pic VALUES(?,?)");
-                ps1.setString(1, name);
+                ps1.setString(1, "ebenezer");
                 if (inputStream != null) {
                     ps1.setBlob(2, inputStream);
                 }
@@ -40,7 +35,7 @@ public class editProfile extends HttpServlet {
                 }
                 String sql2 = "SELECT photo FROM profile_pic WHERE id = ?";
                 PreparedStatement ps2 = con.prepareStatement(sql2);
-                ps2.setString(1, name);
+                ps2.setString(1, "ebenezer");
                 byte [] blob = null;
                 ResultSet rs = ps2.executeQuery();
                 while (rs.next()) {
@@ -55,11 +50,13 @@ public class editProfile extends HttpServlet {
             }
         }
     }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
