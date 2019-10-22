2aa3d043d53160ac4e099258aed59376343879dd
Ebenezer Isaac <ebenezerv99@gmail.com>
2019-10-23 01:34:32 +0530

    profile pic

--- WebApp/src/java/editProfile.java
+++ WebApp/src/java/editProfile.java
@@ -4,18 +4,18 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
-import javax.xml.bind.DatatypeConverter;
 
 @MultipartConfig(maxFileSize = 16177215)
 public class editProfile extends HttpServlet {
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -26,40 +26,46 @@ public class editProfile extends HttpServlet {
             if (filePart != null) {
                 inputStream = filePart.getInputStream();
             }
+            int flag = 0;
             try {
                 Class.forName("com.mysql.cj.jdbc.Driver");
-                Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb?zeroDateTimeBehavior=convertToNull", "root", "");
-                PreparedStatement ps1 = con.prepareStatement("INSERT INTO profile_pic VALUES(?,?)");
-                ps1.setString(1, name);
+                Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cerberus?zeroDateTimeBehavior=convertToNull", "root", "");
+                String sql = "UPDATE faculty SET photo = ? WHERE facultyID=1;";
+                PreparedStatement ps1 = con.prepareStatement(sql);
                 if (inputStream != null) {
-                    ps1.setBlob(2, inputStream);
+                    ps1.setBlob(1, inputStream);
+                } else {
+                    flag = 1;
                 }
-                int row = ps1.executeUpdate();
-                if (row > 0) {
-                    out.print("<h1> File Inserted into database </h1>");
-                }
-                String sql2 = "SELECT photo FROM profile_pic WHERE id = ?";
-                PreparedStatement ps2 = con.prepareStatement(sql2);
-                ps2.setString(1, name);
-                byte [] blob = null;
-                ResultSet rs = ps2.executeQuery();
-                while (rs.next()) {
-                   blob = rs.getBytes("photo");
-                    }
-                    con.close();
-                    System.out.println("FILE SAVED");
-                     String imgString = DatatypeConverter.printBase64Binary(blob);
-                     out.print("<img src=\"data:image/png;base64,"+imgString+"\"/>");
+                ps1.executeUpdate();
+                con.close();
             } catch (ClassNotFoundException | SQLException e) {
                 System.out.println(e);
             }
+            if (flag == 0) {
+                RequestDispatcher rd = request.getRequestDispatcher("message.jsp");
+                request.setAttribute("redirect", "true");
+                request.setAttribute("head", "Request Successfull");
+                request.setAttribute("body", "Your Profile Picture has been changed");
+                request.setAttribute("url", "homepage");
+                rd.forward(request, response);
+            } else {
+                RequestDispatcher rd = request.getRequestDispatcher("message.jsp");
+                request.setAttribute("redirect", "true");
+                request.setAttribute("head", "Request Unsuccessfull");
+                request.setAttribute("body", "New Profile Picture is not supported");
+                request.setAttribute("url", "homepage");
+                rd.forward(request, response);
+            }
         }
     }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
--- WebApp/src/java/homepage.java
+++ WebApp/src/java/homepage.java
@@ -1,11 +1,17 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.xml.bind.DatatypeConverter;
 
 public class homepage extends HttpServlet {
 
@@ -20,6 +26,27 @@ public class homepage extends HttpServlet {
             switch (access) {
                 case 1:
                     request.getRequestDispatcher("side-faculty.html").include(request, response);
+                    try {
+                        Class.forName("com.mysql.cj.jdbc.Driver");
+                        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cerberus?zeroDateTimeBehavior=convertToNull", "root", "");
+                        String sql2 = "SELECT name, photo FROM faculty WHERE facultyID = 1";
+                        PreparedStatement ps2 = con.prepareStatement(sql2);
+                        byte[] blob = null;
+                        String name = "";
+                        ResultSet rs = ps2.executeQuery();
+                        while (rs.next()) {
+                            blob = rs.getBytes("photo");
+                            name = rs.getString("name");
+                        }
+                        con.close();
+                        System.out.println("FILE SAVED");
+                        String imgString = DatatypeConverter.printBase64Binary(blob);
+                        out.print("<script>document.getElementById('pic').innerHTML=\"<a href='editProfilePage.html'><img src='data:image/png;base64," + imgString + "'/><br>"+name+"<br><br></a>\";</script>");
+                    } catch (ClassNotFoundException | SQLException e) {
+                        System.out.println(e);
+                    }
+                    //
+                    //
                     request.getRequestDispatcher("end.html").include(request, response);
                     break;
                 case 0:
