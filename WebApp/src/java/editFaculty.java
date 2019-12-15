e3f6c534a653e364ec1754aff98d4a368da1e6d6
Lokhandwala Haji Huseinali <hajihusen45642@gmail.com>
2019-12-15 17:54:00 +0530

    Email validation in addFaculty servlet

--- WebApp/src/java/editFaculty.java
+++ WebApp/src/java/editFaculty.java
@@ -54,7 +54,7 @@ public class editFaculty extends HttpServlet {
                                 + "<option value='Prof. '>Prof.</option>"
                                 + "</select>"
                                 + "<input type='text' name='name' class=\"editSubjectForm\" placeholder='First Name' maxlength='15'/></td></tr>"
-                                + "<tr><td class=\"editSubjectStyle\">Faculty Email</td><td> : </td><td><input type='email' name='email' style='width: 303.3px' class=\"editSubjectForm\" placeholder='zuck@gmail.com'/></td></tr> "
+                                + "<tr><td class=\"editSubjectStyle\">Faculty Email</td><td> : </td><td><input type='email' name='email' id='email' onchange='checkValidations(0);' style='width: 303.3px' class=\"editSubjectForm\" placeholder='zuck@gmail.com'/></td><td><div id='disp1' class=\"tooltipp\" style='width:100px'><i class=\"fa fa-times\" aria-hidden=\"true\"></i></div></td></tr> "
                                 + "</table><br><button style='width:200px;' type='submit' class='btn btn-primary'>Add Faculty</button></form></div>");
                     } else if (flow.equals("del")) {
                         HttpSession session = request.getSession(false);
